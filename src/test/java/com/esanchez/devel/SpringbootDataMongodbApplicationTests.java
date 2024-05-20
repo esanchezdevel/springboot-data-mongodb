9c31766795277281617c49c5172c7b154e6a8243
Enrique Sanchez <esanchez.devel@gmail.com>
2024-05-20 20:51:26 +0200

    feat: project creation

---
+++ src/test/java/com/esanchez/devel/SpringbootDataMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esanchez.devel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDataMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
